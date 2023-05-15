0de2544cf733a2c1ae0e08129cae40ddff52fb59
gh0styxx <42499915+gh0styxx@users.noreply.github.com>
2023-05-15 09:48:50 +0200

    Add files via upload

---
+++ importEmailCertificate.ps1
@@ -0,0 +1,6 @@
+﻿Expand-Archive -Path C:\Users\$env:UserName\Downloads\certificate-root-ca.zip -DestinationPath C:\Users\$env:UserName\Downloads\ -Force
+Import-Certificate -FilePath "C:\Users\$env:UserName\Downloads\certificate-root-ca.crt" -CertStoreLocation Cert:\CurrentUser\Root
+$mypwd = Get-Credential -UserName $env:UserName -Message 'Enter certificate password below'
+Set-Location -Path "C:\Users\$env:UserName\Downloads"
+$file = Get-ChildItem $dir -File '*.p12'
+Import-PfxCertificate -FilePath $file -CertStoreLocation Cert:\CurrentUser\My -Password $mypwd.Password
